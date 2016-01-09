f5e643f835de358661d82e5134d0cb19fe50e830
Joshua Snider <josh@joshuasnider.com>
2016-01-09 11:43:59 -0500

    Start of the GAE code.

---
+++ src/main/java/myapp/MainServlet.java
@@ -0,0 +1,29 @@
+package myapp;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("{ \"name\": " + getCountryName(0) + " }");
+        resp.getWriter().println("{ \"image\": " + getCountryImage(0) + " }");
+    }
+
+    /**
+     * Get the name of a country given it's unique ID.
+     */
+    private String getCountryName(int id) {
+      return "United States of America";
+    }
+
+    /**
+     * Get a picture of a country given it's unique ID.
+     */
+    private String getCountryImage(int id) {
+      return
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/USA_orthographic.svg/220px-USA_orthographic.svg.png";
+    }
+}
