62ebc609897b418d668709ad7b50645479d5492b
Joshua Snider <josh@joshuasnider.com>
2016-01-17 19:16:54 -0500

    Skeleton for future work.

---
+++ src/main/java/myapp/EloServlet.java
@@ -0,0 +1,20 @@
+
+package myapp;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.http.*;
+import com.google.appengine.api.utils.SystemProperty;
+
+/**
+ * Servlet for calculating and caching the Elo rankings.
+ */
+public class EloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      //TODO
+    }
+
+}
---
+++ src/main/java/myapp/RankServlet.java
@@ -0,0 +1,21 @@
+
+
+package myapp;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.http.*;
+import com.google.appengine.api.utils.SystemProperty;
+
+/**
+ * Servlet for returning the countries and Elo rankings.
+ */
+public class RankServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+      //TODO
+    }
+
+}
